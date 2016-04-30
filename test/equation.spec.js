46e2182e820feed5a67e44c61f71d7025e5c4a00
Richard Hull <rm_hull@yahoo.co.uk>
2016-04-30 01:14:32 +0100

    Fix failing test & add some more cases

--- test/equation.spec.js
+++ test/equation.spec.js
@@ -8,15 +8,27 @@ describe("Equation", () => {
 
   describe("validation", () => {
 
-    ["t+3", "t<<5", "(t+5)*t>>2"].forEach(expr => {
+    ["t+3", "t<<5", "(t+5)*t>>2", "x+y", "1e7"].forEach(expr => {
       it("should allow simple expression: " + expr, () => {
         validate(expr).should.equal(expr);
       });
     });
 
-    ["console.log(", "Math.sin(t)", "x+y"].forEach(expr => {
+    ["console.log(", "Math.sin(t)", "a+b"].forEach(expr => {
       it("should not allow invalid expression: " + expr, () => {
         (() => { validate(expr); }).should.throw("Invalid expression: " + expr);
+      });
+    });
+
+    ["t--2"].forEach(expr => {
+      it("should not allow invalid expression: " + expr, () => {
+        (() => { validate(expr); }).should.throw("Unexpected number");
+      });
+    });
+
+    ["(t*t"].forEach(expr => {
+      it("should not allow invalid expression: " + expr, () => {
+        (() => { validate(expr); }).should.throw("Unexpected end of input");
       });
     });
   });
