3bfac1871271f225b61402ce131893e342669fc6
Richard Hull <rm_hull@yahoo.co.uk>
2017-09-30 17:55:40 +0100

    Add clustering

--- src/app.js
+++ src/app.js
@@ -1,34 +1,50 @@
 import express from "express";
+import cluster from "cluster";
+import _ from "lodash";
 
 import { audioStream } from "./audio-encoder";
 import { validate, compute } from "./equation";
 import { shortLittleEndian, byteClip } from "./byte-converters";
 import { flatMap } from "./generators";
 
-function logErrors(err, req, res, next) {
-  console.error(err.stack);
-  next(err);
+const spawnCount = 16;
+const port = 3000;
+
+function server(port) {
+  const app = express();
+
+  const logErrors = (err, req, res, next) => {
+    console.error(err.stack);
+    next(err);
+  }
+
+  const errorHandler = (err, req, res, next) => {
+    res.status(500);
+    res.render("Error", { error: err });
+  }
+
+  app.use(express.static("public"));
+  app.use(logErrors);
+  app.use(errorHandler);
+
+  app.get("/:hz/:expr", (req, res) => {
+
+    let hz = parseInt(req.params.hz);
+    let expr = validate(req.params.expr);
+    let gen = flatMap(byteClip, compute(expr));
+
+    console.log(`Generating audio/x-wav for ${expr} at ${hz} Hz`);
+
+    res.type("audio/x-wav");
+    audioStream(gen, hz, 8).pipe(res, "binary");
+  });
+
+  app.listen(port);
 }
 
-function errorHandler(err, req, res, next) {
-  res.status(500);
-  res.render("Error", { error: err });
+if (cluster.isMaster) {
+  console.log(`Spawning ${spawnCount} express servers port: ${port}`);
+  _.times(spawnCount, cluster.fork);
+} else {
+  server(port);
 }
-
-const app = express();
-
-app.use(express.static("public"));
-app.use(logErrors);
-app.use(errorHandler);
-
-app.get("/:hz/:expr", (req, res) => {
-
-  let hz = parseInt(req.params.hz);
-  let expr = validate(req.params.expr);
-  let gen = flatMap(byteClip, compute(expr));
-
-  res.type("audio/x-wav");
-  audioStream(gen, hz, 8).pipe(res, "binary");
-});
-
-app.listen(3000);
