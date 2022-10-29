3770b476b62fc733fcd95a2b997b81dbb0d424c7
Richard Hull <rm_hull@yahoo.co.uk>
2022-10-29 20:42:09 +0100

    Namespaced local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { Route, Routes, useParams } from "react-router-dom";
-
-const theme = {
-  style: {
-    global: {},
-  },
-};
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Routes>
-        <Route
-          path="/about"
-          element={
-            <div>
-              <h3>About page</h3>
-            </div>
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <div>
-              <h3>Home page</h3>
-            </div>
-          }
-        />
-        <Route path="/:id" element={<Child />} />
-      </Routes>
-    </ChakraProvider>
+    <Routes>
+      <Route
+        path="/about"
+        element={
+          <div>
+            <h3>About page</h3>
+          </div>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <div>
+            <h3>Home page</h3>
+          </div>
+        }
+      />
+      <Route path="/:id" element={<Child />} />
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
+import { ChakraProvider, ColorModeScript, createLocalStorageManager, theme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+const manager = createLocalStorageManager("8-bit-trip.color-mode");
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Router basename="/8-bit-trip">
-      <App />
-    </Router>
+    <ColorModeScript initialColorMode="dark" storageKey="xchg-rax-rax.color-mode" />
+    <ChakraProvider theme={theme} colorModeManager={manager}>
+      <Router basename="/8-bit-trip">
+        <App />
+      </Router>
+    </ChakraProvider>
   </React.StrictMode>
 );
