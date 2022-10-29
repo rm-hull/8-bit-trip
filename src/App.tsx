3bade5ec17ddf5e845f78e1b94c078dc2d5e7f76
Richard Hull <rm_hull@yahoo.co.uk>
2022-10-29 20:38:50 +0100

    Add chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { Route, Routes, useParams } from "react-router-dom";
+
+const theme = {
+  style: {
+    global: {},
+  },
+};
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/about"
-        element={
-          <div>
-            <h3>About page</h3>
-          </div>
-        }
-      />
-      <Route
-        path="/"
-        element={
-          <div>
-            <h3>Home page</h3>
-          </div>
-        }
-      />
-      <Route path="/:id" element={<Child />} />
-    </Routes>
+    <ChakraProvider theme={theme}>
+      <Routes>
+        <Route
+          path="/about"
+          element={
+            <div>
+              <h3>About page</h3>
+            </div>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <div>
+              <h3>Home page</h3>
+            </div>
+          }
+        />
+        <Route path="/:id" element={<Child />} />
+      </Routes>
+    </ChakraProvider>
   );
 }
 
