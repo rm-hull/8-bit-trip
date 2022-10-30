29bcf65cf14339328e8ed22a1da73199da0b0dbf
Richard Hull <rm_hull@yahoo.co.uk>
2022-10-30 23:58:05 +0000

    Fade effect

--- src/visualizations/sineWave.ts
+++ src/visualizations/sineWave.ts
@@ -6,8 +6,9 @@ export function sineWave(canvas: HTMLCanvasElement, data: Uint8Array) {
   const sliceWidth = (width * 1.0) / data.length;
 
   context.lineWidth = 2;
-  context.strokeStyle = "#000000";
-  context.clearRect(0, 0, width, height);
+  context.strokeStyle = "#404040";
+  context.fillStyle = "rgba(255, 255, 255, 0.25)";
+  context.fillRect(0, 0, width, height);
 
   context.beginPath();
   context.moveTo(0, height / 2);
