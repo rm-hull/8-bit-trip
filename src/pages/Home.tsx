043e578c8079fe2bd817ed0461f097c3c79d40c3
Richard Hull <rm_hull@yahoo.co.uk>
2022-10-30 23:59:27 +0000

    Pass algorithm into audo worklet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { FiPlay, FiSquare } from "react-icons/fi";
 import AudioAnalyzer from "../components/AudioAnalyzer";
 
-async function playNoise(context: AudioContext): Promise<AudioWorkletNode> {
+async function playNoise(context: AudioContext, algorithm: string): Promise<AudioWorkletNode> {
   try {
     await context.audioWorklet.addModule("js/audio-processor.js");
   } catch (err: unknown) {
@@ -11,7 +11,12 @@ async function playNoise(context: AudioContext): Promise<AudioWorkletNode> {
     console.warn("Failed to load audio-worklet, trying dev fallback...", { err });
     await context.audioWorklet.addModule("8-bit-trip/js/audio-processor.js");
   }
-  const audioNode = new AudioWorkletNode(context, "audio-processor");
+  const audioNode = new AudioWorkletNode(context, "audio-processor", {
+    processorOptions: {
+      samepleRate: context.sampleRate,
+      algorithm,
+    },
+  });
   audioNode.connect(context.destination);
   return audioNode;
 }
@@ -27,7 +32,34 @@ export default function Home(): JSX.Element {
     const ctx = new AudioContext({ sampleRate });
     setContext(ctx);
 
-    const node = await playNoise(ctx);
+    const node = await playNoise(
+      ctx,
+      "t>>4|t&(t>>5)/(t>>7-(t>>15)&-t>>7-(t>>15))"
+      // "(t/((t>>16|t>>8))&((t>>5|t>>11)))-1|t*((t>>16|t>>8))"
+      // "t*((t>>3|t>>9)&82&t>>9)"
+      // "t*((t+13217)/1211)&(t>>2|t>>4|t>>6)/512"
+      // "(t|(t>>9|t>>7))*t&(t>>11|t>>9)"
+      // "(t>>7|t|t>>6)*10+4*(t&t>>13|t>>6)"
+      // "((-t&4095)*(255&t*(t&t>>13))>>12)+(127&t*(234&t>>8&t>>3)>>(3&t>>14))"
+      // "(t/10000000*t*t+t)%127|t>>3"
+      // "(t&t%255)-(t*3&t>>13&t>>6)"
+      // "v=(v>>1)+(v>>4)+t*(((t>>16)|(t>>6))&(69&(t>>9)))"
+      // "(t<65536)?((2*t*(t>>11)&(t- 1)|(t>>4)-1)%64):(((t%98304)>65536)?((17*t*(2*t>>8)&(t-1)|(t>>6)-1)%64|(t>>4)):((15*t*(2*t>>16)&(t-1)|(t>>8)-1)%64|(t>>4)))"
+      // "t>>6^t&37|t+(t^t>>11) -t*((t%24?2:6)&t>>11)^t<<1 &(t&598?t>>4:t>>10)"
+      // "t*(t^t+(t>>15|1)^(t-1280^t)>>10)"
+      // "(t*9&t>>4|t*5&t>>7|t*3&t/1024)-1"
+      // "(t|t>>5)&(t|t>>13)-(t|t>>21)"
+      // "(t*(t>>5|t>>8))>>(t>>16)"
+      // "(t%(t/(t>>9|t>>13)))"
+      // "((t*(t>>8|t>>9)&46&t>>8))^(t&t>>13|t>>6)"
+      // "((-t&4095)*(255&t*(t&t>>13))>>12)+(127&t*(234&t>>8&t>>3)>>(3&t>>14))"
+      // "(t*(t>>8*(t>>15|t>>8)&(20|(t>>19)*5>>t|t>>3)))"
+      // "(t>>6|t|t>>(t>>16))*10+((t>>11)&7)",
+      // "((1-(((t+10)>>((t>>9)&((t>>14))))&(t>>4&-2)))*2)*(((t>>10)^((t+((t>>6)&127))>>10))&1)*32+128"
+      // "16 * t*t* (t >>11)/7"
+      // "(t|t>>5)&(t|t>>13)-(t|t>>21)"
+      // "t>>4|t&(t>>5)/(t>>7-(t>>15)&-t>>7-(t>>15))"
+    );
     const streamNode = ctx.createMediaStreamDestination();
     node.connect(streamNode);
 
