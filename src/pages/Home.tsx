94d0087ff67aba5c0685136ea994d05104b37265
Richard Hull <rm_hull@yahoo.co.uk>
2022-10-30 01:03:42 +0000

    Playing something!

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,32 @@
 import { Button, ButtonGroup, Container } from "@chakra-ui/react";
 import { useState } from "react";
-import { FiPause, FiPlay, FiSquare } from "react-icons/fi";
+import { FiPlay, FiSquare } from "react-icons/fi";
 import AudioAnalyzer from "../components/AudioAnalyzer";
+
+async function playNoise(context: AudioContext): Promise<AudioWorkletNode> {
+  await context.audioWorklet.addModule("8-bit-trip/js/audio-processor.js");
+  const audioNode = new AudioWorkletNode(context, "audio-processor");
+  audioNode.connect(context.destination);
+  return audioNode;
+}
 
 export default function Home(): JSX.Element {
   const [audio, setAudio] = useState<MediaStream | null>(null);
+  const [node, setNode] = useState<AudioWorkletNode | null>(null);
   const isPlaying = !!audio;
 
   const start = async () => {
-    const media = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: false,
-    });
+    const context = new AudioContext();
+    const node = await playNoise(context);
+    const streamNode = context.createMediaStreamDestination();
+    node.connect(streamNode);
 
-    setAudio(media);
+    setAudio(streamNode.stream);
+    setNode(node);
   };
 
   const stop = () => {
+    node?.disconnect();
     audio?.getTracks().forEach((track) => track.stop());
     setAudio(null);
   };
@@ -34,9 +44,6 @@ export default function Home(): JSX.Element {
       <ButtonGroup size="sm" isAttached variant="outline">
         <Button leftIcon={isPlaying ? <FiSquare /> : <FiPlay />} onClick={toggle}>
           {isPlaying ? "Stop" : "Start"}
-        </Button>
-        <Button leftIcon={<FiPause />} onClick={stop}>
-          Pause
         </Button>
       </ButtonGroup>
       {isPlaying ? <AudioAnalyzer audioStream={audio} /> : undefined}
